04195ad3e731261c79584c23deb0ae29217807a5
mdhira <mdhera891@gmail.com>
2021-04-12 23:57:23 -0500

    make all script

---
+++ Python_Graph_Design.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Data-set1.csv')
+
+
+
+AB = df[df['Province'] == 'AB']
+MB = df[df['Province'] == 'MB']
+PEI = df[df['Province'] == 'PEI']
+NB = df[df['Province'] == 'NB']
+ON = df[df['Province'] == 'ON']
+SK = df[df['Province'] == 'SK']
+NL = df[df['Province'] == 'NL']
+QC = df[df['Province'] == 'QC']
+BC = df[df['Province'] == 'BC']
+NS = df[df['Province'] == 'NS']
+
+plt.plot(AB['Year'],AB['NX Interprovincial Services'])
+plt.plot(MB['Year'],MB['NX Interprovincial Services'])
+plt.plot(PEI['Year'],PEI['NX Interprovincial Services'])
+plt.plot(NB['Year'],NB['NX Interprovincial Services'])
+plt.plot(ON['Year'],ON['NX Interprovincial Services'])
+plt.plot(SK['Year'],SK['NX Interprovincial Services'])
+plt.plot(NL['Year'],NL['NX Interprovincial Services'])
+plt.plot(QC['Year'],QC['NX Interprovincial Services'])
+plt.plot(BC['Year'],BC['NX Interprovincial Services'])
+plt.plot(NS['Year'],NS['NX Interprovincial Services'])
+plt.grid(True)
+plt.legend(['Alberta','Manitoba','Prince Edward','New Brunswick','Ontario','Saskachuwan','New Foundland','Qubec','Britch Colm','Nova Scotia'])
+plt.show()
